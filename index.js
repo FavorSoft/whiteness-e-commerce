d5923cd16cfcbe49751301675856113f379e31e1
Andrii <pinoplastiks@gmail.com>
2017-01-16 11:55:06 +0200

    Configured routing

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')  
+const app = express()  
+const path = require('path')    
+const exphbs = require('express-handlebars')
+
+const port = 3000
+
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')  
+app.set('views', path.join(__dirname, 'views'))  
+
+app.get('/', (request, response) => {  
+  response.render('home', {
+    name: 'John'
+  })
+})
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})

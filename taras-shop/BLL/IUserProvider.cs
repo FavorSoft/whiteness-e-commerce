939456c49980aa3f2ded9f1315bff6f0a94c9741
= <=>
2017-01-24 20:56:56 +0200

    Add User and Role provider

---
+++ taras-shop/BLL/IUserProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO.Helpers;
+namespace BLL
+{
+    public interface IUserProvider
+    {
+        IQueryable<Users> GetAllUsers();
+        Users GetUserById(int id);
+        void AddUser(Users user);
+        void EditUser(Users user);
+        void DeleteUser(int id);
+    }
+}

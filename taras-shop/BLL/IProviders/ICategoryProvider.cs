8059c0a6ecf5464bcd8fbc883b6fe3d1d10af9c8
= <andrii.kolomys@gmail.com>
2017-01-27 15:36:54 +0200

    Start work with BLL

---
+++ taras-shop/BLL/IProviders/ICategoryProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public interface ICategoryProvider
+    {
+        void AddItem(DTO.Categories category);
+        List<DTO.Categories> GetAll();
+        DTO.Categories GetById(int id);
+    }
+}
---
+++ taras-shop/BLL/Providers/CategoryProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL.Repositories;
+namespace BLL.Providers
+{
+    public class CategoryProvider : ICategoryProvider
+    {
+        DAL.ICategoriesRepository _repo;
+        public CategoryProvider()
+        {
+            _repo = new DAL.Repositories.CategoriesRepository();
+        }
+        public void AddItem(Categories category)
+        {
+            _repo.AddItem(new DAL.Categories()
+            {
+                category = category.Category,
+                category_img = category.CategoryImg,
+                type_id = category.TypeId,
+                description = category.Description   
+            });
+        }
+
+        public List<Categories> GetAll()
+        {
+            return ConvertModeltoDTO(_repo.GetAll());
+        }
+        List<Categories> ConvertModeltoDTO(IQueryable<DAL.Categories> repo)
+        {
+            List<Categories> res = new List<Categories>();
+            foreach(var i in repo)
+            {
+                res.Add(new Categories()
+                {
+                    Id = i.id,
+                    Category = i.category,
+                    CategoryImg = i.category_img,
+                    Description = i.description,
+                    TypeId = i.type_id
+                });
+            }
+            return res;
+        }
+
+        public Categories GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

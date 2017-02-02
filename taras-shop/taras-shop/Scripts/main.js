dfacc941483ff0c34ec8083615a61c2f21745878
DennnyGreb <dendendengrebenets@gmail.com>
2017-02-02 13:07:45 +0200

    Fix js bug

--- taras-shop/taras-shop/Scripts/main.js
+++ taras-shop/taras-shop/Scripts/main.js
@@ -1,12 +1,12 @@
-﻿$( function() {
-    $( "#accordion" ).accordion(
+﻿$(function () {
+    $("#accordion").accordion(
         {
             collapsible: true,
             animate: 150,
             heightStyle: "content"
         }
     );
-} );
+});
 
 $(function () {
     $("#slider").slider({
@@ -27,45 +27,20 @@ $(function () {
     $('#accordion').removeClass('ui-accordion');
 });
 
-window.onload = function () {
-    console.log("Hello111");
-    function appendUl() {
-        console.log("hello");
-        var ulToAppend = "<h5>После регистрации:</h5>"
-        + "<ul class='after-reg'>"
-        + "<li>Вы используете привилегии (VIP-скидки, ночные распродажи и т.д.)</li>"
-        + "<li>Для зарегистрированных Клиентов есть возможность бесплатной доставки</li>"
-        + "<li>Вы имеете доступ к истории своих покупок и хранению избранных товаров</li>"
-        + "</ul>";
-        $(".why-reg-block").append(ulToAppend);
-    }
-    var el = document.querySelector(".why-reg");
-    el.addEventListener("click", appendUl, false);
-}
-
 $(document).ready(function () {
-    console.log("Hello1");
     $(".why-reg").click(function () {
-        console.log("Hello");
-        var ulToAppend = "<h5>После регистрации:</h5>"
-            + "<ul class='after-reg'>"
-                + "<li>Вы используете привилегии (VIP-скидки, ночные распродажи и т.д.)</li>"
-                + "<li>Для зарегистрированных Клиентов есть возможность бесплатной доставки</li>"
-                + "<li>Вы имеете доступ к истории своих покупок и хранению избранных товаров</li>"
-            + "</ul>";
-
-        $(ulToAppend).appendTo(".why-reg-block");
+        if ($(".why-reg-content").html() == "") {
+            var ulToAppend = "<h5>После регистрации:</h5>"
+                + "<ul class='after-reg'>"
+                    + "<li>Вы используете привилегии (VIP-скидки, ночные распродажи и т.д.)</li>"
+                    + "<li>Для зарегистрированных Клиентов есть возможность бесплатной доставки</li>"
+                    + "<li>Вы имеете доступ к истории своих покупок и хранению избранных товаров</li>"
+                + "</ul>";
+            $(ulToAppend).appendTo(".why-reg-content");
+        }
+        else {
+            $(".why-reg-content").html("");
+        }
     });
+
 });
-
-var clickSpan = function(){
-    console.log("Hello");
-    var ulToAppend = "<h5>После регистрации:</h5>"
-        + "<ul class='after-reg'>"
-            + "<li>Вы используете привилегии (VIP-скидки, ночные распродажи и т.д.)</li>"
-            + "<li>Для зарегистрированных Клиентов есть возможность бесплатной доставки</li>"
-            + "<li>Вы имеете доступ к истории своих покупок и хранению избранных товаров</li>"
-        + "</ul>";
-
-    $(ulToAppend).appendTo(".why-reg-block");
-}

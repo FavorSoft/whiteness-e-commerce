b96bf42552d19c1f9c317ce14fe5b19417056177
DennnyGreb <dendendengrebenets@gmail.com>
2017-02-21 22:39:43 +0200

    Change components to es6

--- taras-shop/taras-shop/Scripts/jsx/Greeting.jsx
+++ taras-shop/taras-shop/Scripts/jsx/Greeting.jsx
@@ -1,13 +1,13 @@
-﻿var Greeting = React.createClass({
-    render: function() {
+﻿class Greeting extends React.Component {
+    render() {
         return (
           <div className="greeting-block">
             Hello, world! I am a GreetingBox.
           </div>
       );
     }
-});
+};
 ReactDOM.render(
   <Greeting/>,
-  document.getElementById('content')
+  document.getElementById('greet')
 );
--- taras-shop/taras-shop/Scripts/jsx/Index.jsx
+++ taras-shop/taras-shop/Scripts/jsx/Index.jsx
@@ -1,12 +1,12 @@
-﻿var CommentBox = React.createClass({
-    render: function() {
+﻿class CommentBox extends React.Component {
+    render() {
         return (
           <div className="commentBox">
             Hello, world! I am a CommentBox.
           </div>
       );
     }
-});
+};
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')

e26b80af681571ff6628b59ebaed5d33edb37889
= <andrii.kolomys@gmail.com>
2017-03-15 11:52:27 +0200

    Add new DAL for local DB

---
+++ taras-shop/LocalDAL/Identity/IdentityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Identity
+{
+    class IdentityContext : DbContext
+    {
+        public IdentityContext() :
+            base("DefaultConnection")
+        { }
+
+        public DbSet<UsersIdentity> Users { get; set; }
+
+    }
+}
---
+++ taras-shop/LocalDAL/Repositories/BasketItemsRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.IRepository;
+namespace DAL.Repository
+{
+    public class BasketItemsRepository : IRepository<Basket_items>
+    {
+        LocalEntities entities;
+        public BasketItemsRepository(LocalEntities db)
+        {
+            entities = db;
+        }
+        public void AddItem(Basket_items item)
+        {
+            entities.Basket_items.Add(item);
+        }
+
+        public void DeleteItem(int id)
+        {
+            var item = entities.Basket_items.FirstOrDefault(x => x.id == id);
+            entities.Basket_items.Remove(item);
+            entities.Entry(item).State = System.Data.Entity.EntityState.Deleted;
+        }
+
+        public void EditItem(Basket_items item)
+        {
+            entities.Basket_items.Attach(item);
+            entities.Entry(item).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        public IQueryable<Basket_items> GetAll()
+        {
+            return entities.Basket_items;
+        }
+
+        public Basket_items GetById(int id)
+        {
+            return entities.Basket_items.Where(x => x.id == id).FirstOrDefault();
+        }
+    }
+}

bf24519aedf3d55eefa4bdb22ff03095c505f0d2
Andrii Kolomys <Andrii Kolomys>
2017-03-29 23:03:14 +0300

    Chenge sql file

---
+++ taras-shop/taras-shop/Models/ItemOnBasket.cs
@@ -0,0 +1,13 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace taras_shop.Models
+{
+    public class ItemOnBasket : UnitDto
+    {
+        public string Image { get; set; }
+    }
+}

4fd4db80856e11044d6c90e68adb699fd9a64d16
Andrij <pinoplastiks@gmail.com>
2017-04-19 12:44:59 +0300

    configure projet for begin work

--- taras-shop/DALLocalDB/Model1.Context.cs
+++ taras-shop/DALLocalDB/Model1.Context.cs
@@ -9,10 +9,9 @@
 
 namespace DALLocalDB
 {
-    using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-    
+
     public partial class LocalEntities : DbContext
     {
         public LocalEntities()
